b90a5e8680bd7e421f09be85c9a1859d3564bf15
adminadmin <git@akki.uk>
2026-01-08 14:16:38 +0000

    about to break everything

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import { type MouseEvent, type TouchEvent, useCallback, useEffect, useRef, useState } from 'react';
-import { Point, Vector } from '@/lib/2d';
+import { Point, type PointLike, Vector } from '@/lib/2d';
 import { clamp } from '@/lib/utils';
 
 interface Circle {
@@ -9,13 +9,13 @@ interface Circle {
   hue: number;
   radius: number;
   opacity: number;
-  collisions: Vector[];
-  static: boolean;
+  nextCollision: Point | null;
+  idleCount: number;
 }
 
 const GRAVITY = new Vector({ x: 0, y: 0.1 });
 const BOUNCE_VELOCITY_RATIO = 0.8;
-const GRAVITY_DAMPING_VALUE = 0.2;
+const GRAVITY_DAMPING_VALUE = 0.05;
 
 function addCircle(items: Circle[], x: number, y: number, left?: boolean) {
   const radius = Math.round(Math.random() * 20 + 10);
@@ -29,10 +29,48 @@ function addCircle(items: Circle[], x: number, y: number, left?: boolean) {
     hue: Math.round(Math.random() * 360),
     radius,
     opacity: 100,
-    collisions: [],
     nextPosition: null,
-    static: false,
+    nextCollision: null,
+    idleCount: 0,
   });
+}
+
+function drawCircle(context: CanvasRenderingContext2D, item: Circle) {
+  context.beginPath();
+  context.arc(item.position.x, item.position.y, item.radius, 0, Math.PI * 2);
+  context.closePath();
+  context.fillStyle = `hsl(${item.hue} 100 50 / ${item.opacity}%)`;
+  context.fill();
+  context.save();
+  context.clip();
+
+  // highlight
+  // context.beginPath();
+  // context.arc(item.position.x, item.position.y, item.radius, Math.PI * 0.75, Math.PI * 1.75);
+  // context.arc(item.position.x + item.radius * 0.8, item.position.y + item.radius * 0.8, item.radius * 1.5, Math.PI * 1.75, Math.PI * 0.75, true);
+  // context.closePath();
+  // context.fillStyle = `hsl(${item.hue} 100 80 / ${item.opacity}%)`;
+  // context.fill();
+
+  // shadow
+  context.beginPath();
+  context.arc(item.position.x, item.position.y, item.radius, Math.PI * -0.25, Math.PI * 0.75);
+  context.arc(item.position.x - item.radius * 0.8, item.position.y - item.radius * 0.8, item.radius * 1.5, Math.PI * 0.75, Math.PI * -0.25, true);
+  context.closePath();
+  context.fillStyle = `hsl(${item.hue} 100 30 / ${item.opacity}%)`;
+  context.fill();
+
+  context.restore();
+}
+
+function initCanvas(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D): PointLike {
+  const rect = canvas.getBoundingClientRect();
+  const round = Point.round({ x: rect.width, y: rect.height });
+  canvas.width = round.x;
+  canvas.height = round.y;
+  context.fillStyle = '#000';
+  context.fillRect(0, 0, round.x, round.y);
+  return round;
 }
 
 export default function App() {
@@ -42,111 +80,116 @@ export default function App() {
 
   // biome-ignore lint/correctness/useExhaustiveDependencies(reload): deliberate
   useEffect(() => {
-    const loopController = new AbortController();
-    const listenerController = new AbortController();
     if (!canvasRef.current) throw new Error('oh no');
     const context = canvasRef.current.getContext('2d');
     if (!context) throw new Error('oh no');
+    const controller = new AbortController();
     let prevTime = 0;
-    document.addEventListener(
-      'keydown',
-      (event) => {
-        if (event.key === 'r') setReload(Math.random());
-        if (event.key === 'd') console.log(itemsRef.current);
-        if (event.key === 'c') itemsRef.current = [];
-        if (event.key === 's') {
-          if (!loopController.signal.aborted) loopController.abort();
-          step(prevTime + 1);
-        }
-      },
-      { signal: listenerController.signal }
-    );
+    let paused = false;
 
-    function step(time: number) {
-      if (!loopController.signal.aborted) requestAnimationFrame(step);
-      if (!canvasRef.current) return;
-      if (!context) return;
+    // biome-ignore format: no
+    document.addEventListener('keydown', (event) => {
+      if (event.key === 'r') setReload(Math.random());
+      if (event.key === 'd') console.log(itemsRef.current);
+      if (event.key === 'c') itemsRef.current = [];
+      if (event.key === 's') {
+        paused = true;
+        step(prevTime + 1, true);
+      }
+      if (event.key === 'p') {
+        paused = !paused;
+        if (!paused) prevTime = 0;
+      }
+      if (event.key === 'f') for (const item of itemsRef.current) item.idleCount = 10;
+      if (event.key === 'u') for (const item of itemsRef.current) item.idleCount = 0;
+      console.debug({ paused, prevTime });
+    }, { signal: controller.signal });
+
+    function step(time: number, force = false) {
+      if (!controller.signal.aborted) requestAnimationFrame(step);
+      if (paused && !force) return;
+      if (!canvasRef.current) throw new Error('oh no');
+      if (!context) throw new Error('oh no');
       const timeDelta = prevTime ? time - prevTime : 0;
-      // const timeDelta = 1;
       prevTime = time;
 
-      const rect = canvasRef.current.getBoundingClientRect();
-      canvasRef.current.width = rect.width;
-      canvasRef.current.height = rect.height;
-      context.fillStyle = '#000';
-      context.fillRect(0, 0, rect.width, rect.height);
+      const bounds = initCanvas(canvasRef.current, context);
 
       for (let i = 0; i < itemsRef.current.length; ++i) {
         const item = itemsRef.current[i];
-        if (item.static) {
-          item.nextPosition = item.position;
-        } else {
+        item.nextCollision = null;
+        if (item.idleCount >= 5) item.nextPosition = null;
+        else {
           item.velocity.addEq(GRAVITY);
           item.nextPosition = item.position.add(item.velocity.mult(timeDelta));
-          item.collisions = [];
+        }
+      }
 
-          const hitX = item.nextPosition.x <= item.radius ? item.radius : item.nextPosition.x >= rect.width - item.radius ? rect.width - item.radius : null;
-          const hitY = item.nextPosition.y <= item.radius ? item.radius : item.nextPosition.y >= rect.height - item.radius ? rect.height - item.radius : null;
-          const intersectRatio = hitX
-            ? clamp((hitX - item.position.x) / (item.nextPosition.x - item.position.x), 0, 1)
-            : hitY
-              ? clamp((hitY - item.position.y) / (item.nextPosition.y - item.position.y), 0, 1)
-              : null;
+      for (let i = 0; i < itemsRef.current.length; ++i) {
+        const item = itemsRef.current[i];
 
-          if (intersectRatio !== null) {
-            const remainRatio = 1 - intersectRatio;
-            const boxIntersect = item.position.add(item.velocity.mult(timeDelta * clamp(intersectRatio, 0, 1)));
+        // null nextPosition means item is idle
+        // TODO: enable collision between static and active circle later
+        if (item.nextPosition !== null) {
+          // skip if we already have a nextCollision
+          for (let o = i + 1; item.nextCollision === null && o < itemsRef.current.length; ++o) {
+            // TODO: try to switch to a path-based rather than intersection-based solution later
+            const other = itemsRef.current[o];
+            // ignore all static for now
+            if (other.nextPosition === null) continue;
+            const hypot = item.nextPosition.hypot2(other.nextPosition);
+            const r2 = (item.radius + other.radius) ** 2;
+            if (hypot < r2) {
+              // FIXME: need the actual collision point
+              item.nextCollision = item.nextPosition;
+              other.nextCollision = other.nextPosition;
+            }
+          }
 
-            item.velocity.x *= typeof hitX === 'number' ? -BOUNCE_VELOCITY_RATIO : BOUNCE_VELOCITY_RATIO;
-            item.velocity.y *= typeof hitY === 'number' ? -BOUNCE_VELOCITY_RATIO : BOUNCE_VELOCITY_RATIO;
-            if (typeof hitY === 'number' && item.velocity.y < 0) item.velocity.y = Math.min(0, item.velocity.y + GRAVITY_DAMPING_VALUE);
+          if (item.nextCollision) {
+            //FIXME: need to reflect velocity along the collision normal
+            item.velocity.x *= -BOUNCE_VELOCITY_RATIO;
+            item.velocity.y *= -BOUNCE_VELOCITY_RATIO;
+            //FIXME: nextPosition should be collision point + (reflected velocity * remaining time)
+            // item.nextPosition = item.position;
+            item.nextPosition.addEq(item.velocity.mult(timeDelta));
+          } else {
+            // for now, bounds collision is only valid if we haven't hit another circle
+            // TODO: a bounce from another circle could knock us oob
+            const hitX = item.nextPosition.x <= item.radius ? item.radius : item.nextPosition.x >= bounds.x - item.radius ? bounds.x - item.radius : null;
+            const hitY = item.nextPosition.y <= item.radius ? item.radius : item.nextPosition.y >= bounds.y - item.radius ? bounds.y - item.radius : null;
+            const intersectRatio = hitX
+              ? clamp((hitX - item.position.x) / (item.nextPosition.x - item.position.x), 0, 1)
+              : hitY
+                ? clamp((hitY - item.position.y) / (item.nextPosition.y - item.position.y), 0, 1)
+                : null;
 
-            item.nextPosition = boxIntersect.add(item.velocity.mult(timeDelta * remainRatio));
+            if (intersectRatio !== null) {
+              const remainRatio = 1 - intersectRatio;
+              const boundsIntersection = item.position.add(item.velocity.mult(timeDelta * clamp(intersectRatio, 0, 1)));
 
-            console.debug('boxIntersect', boxIntersect, 'ratio', intersectRatio, 'next', item.nextPosition);
+              item.velocity.x *= typeof hitX === 'number' ? -BOUNCE_VELOCITY_RATIO : BOUNCE_VELOCITY_RATIO;
+              item.velocity.y *= typeof hitY === 'number' ? -BOUNCE_VELOCITY_RATIO : BOUNCE_VELOCITY_RATIO;
+              if (typeof hitY === 'number' && item.velocity.y < 0) item.velocity.y = Math.min(0, item.velocity.y + GRAVITY_DAMPING_VALUE);
+
+              item.nextPosition = boundsIntersection.add(item.velocity.mult(timeDelta * remainRatio));
+
+              console.debug('boundsIntersection', boundsIntersection, 'ratio', intersectRatio, 'next', item.nextPosition);
+            }
           }
 
           const rounded = item.nextPosition.round();
-          if (rounded.eq(item.position)) item.static = true;
+          if (rounded.eq(item.position)) ++item.idleCount;
           item.position = rounded;
         }
 
-        context.beginPath();
-        context.arc(item.position.x, item.position.y, item.radius, 0, Math.PI * 2);
-        context.closePath();
-        context.fillStyle = `hsl(${item.hue} 100 50 / ${item.opacity}%)`;
-        context.fill();
-        context.save();
-        context.clip();
-
-        // highlight
-        // context.beginPath();
-        // context.arc(item.position.x, item.position.y, item.radius, Math.PI * 0.75, Math.PI * 1.75);
-        // context.arc(item.position.x + item.radius * 0.8, item.position.y + item.radius * 0.8, item.radius * 1.5, Math.PI * 1.75, Math.PI * 0.75, true);
-        // context.closePath();
-        // context.fillStyle = `hsl(${item.hue} 100 80 / ${item.opacity}%)`;
-        // context.fill();
-
-        // shadow
-        context.beginPath();
-        context.arc(item.position.x, item.position.y, item.radius, Math.PI * -0.25, Math.PI * 0.75);
-        context.arc(item.position.x - item.radius * 0.8, item.position.y - item.radius * 0.8, item.radius * 1.5, Math.PI * 0.75, Math.PI * -0.25, true);
-        context.closePath();
-        context.fillStyle = `hsl(${item.hue} 100 30 / ${item.opacity}%)`;
-        context.fill();
-
-        context.restore();
+        drawCircle(context, item);
       }
-
-      itemsRef.current = itemsRef.current.filter((item) => item.position.y < rect.height);
     }
 
     requestAnimationFrame(step);
 
-    return () => {
-      loopController.abort();
-      listenerController.abort();
-    };
+    return () => controller.abort();
   }, [reload]);
 
   const handleMouseMove = useCallback((event: MouseEvent<HTMLCanvasElement>) => {
--- src/lib/2d.tsx
+++ src/lib/2d.tsx
@@ -17,7 +17,7 @@ abstract class PointOrVector<Interface extends PointOrVectorLike> {
     if (params.length === 2 && typeof params[0] === 'number' && typeof params[1] === 'number') {
       this.x = params[0];
       this.y = params[1];
-    } else if (params.length === 1 && typeof params[0] === 'object' && params[0] !== null && 'x' in params[0] && 'y' in params[0]) {
+    } else if (params.length === 1 && PointOrVector.is(params[0])) {
       this.x = params[0].x;
       this.y = params[0].y;
     } else throw new Error('invalid constructor params');
@@ -97,11 +97,11 @@ abstract class PointOrVector<Interface extends PointOrVectorLike> {
     return this;
   }
 
-  hypot2(): number {
-    return PointOrVector.hypot2(this);
+  hypot2(other?: Interface): number {
+    return other ? PointOrVector.hypot2(this, other) : PointOrVector.hypot2(this);
   }
-  hypot(): number {
-    return PointOrVector.hypot(this);
+  hypot(other?: Interface): number {
+    return other ? PointOrVector.hypot(this, other) : PointOrVector.hypot(this);
   }
   eq(other: Interface): boolean {
     return PointOrVector.eq(this, other);
@@ -134,14 +134,22 @@ abstract class PointOrVector<Interface extends PointOrVectorLike> {
   static clamp(item: PointOrVectorLike, min: PointOrVectorLike, max: PointOrVectorLike): PointOrVectorLike {
     return { x: Math.min(Math.max(min.x, item.x), max.x), y: Math.min(Math.max(min.y, item.y), max.y) };
   }
-  static hypot2(item: PointOrVectorLike): number {
-    return item.x ** 2 + item.y ** 2;
+  static hypot2(...params: [item: PointOrVectorLike] | [item: PointOrVectorLike, other: PointOrVectorLike]): number {
+    if (params.length === 1) return params[0].x ** 2 + params[0].y ** 2;
+    if (params.length === 2) return (params[1].x - params[0].x) ** 2 + (params[1].y - params[0].y) ** 2;
+    throw new Error('invalid params');
   }
-  static hypot(item: PointOrVectorLike): number {
-    return Math.sqrt(PointOrVector.hypot2(item));
+  static hypot(...params: [item: PointOrVectorLike] | [item: PointOrVectorLike, other: PointOrVectorLike]): number {
+    if (params.length === 1) return Math.sqrt(params[0].x ** 2 + params[0].y ** 2);
+    if (params.length === 2) return Math.sqrt((params[1].x - params[0].x) ** 2 + (params[1].y - params[0].y) ** 2);
+    throw new Error('invalid params');
   }
   static eq(item: PointOrVectorLike, other: PointOrVectorLike): boolean {
     return item.x === other.x && item.y === other.y;
+  }
+
+  static is(item: unknown): item is PointOrVectorLike {
+    return typeof item === 'object' && item !== null && 'x' in item && 'y' in item && typeof item.x === 'number' && typeof item.y === 'number';
   }
 }
 
@@ -151,8 +159,89 @@ export class Vector extends PointOrVector<VectorLike> {
   unit(): Vector {
     return new Vector(Vector.unit(this));
   }
-  static unit(item: PointOrVectorLike): PointOrVectorLike {
+  rotate(radians: number): Vector {
+    return new Vector(Vector.rotate(this, radians));
+  }
+
+  unitEq(): this {
+    const { x, y } = Vector.unit(this);
+    [this.x, this.y] = [x, y];
+    return this;
+  }
+  rotateEq(radians: number): this {
+    const { x, y } = Vector.rotate(this, radians);
+    [this.x, this.y] = [x, y];
+    return this;
+  }
+
+  static unit(item: VectorLike): VectorLike {
     const hypot = PointOrVector.hypot(item);
     return { x: item.x / hypot, y: item.y / hypot };
+  }
+  static rotate(item: VectorLike, radians: number): VectorLike {
+    // https://www.geeksforgeeks.org/maths/rotation-matrix/
+    const [cos, sin] = [Math.cos(radians), Math.sin(radians)];
+    return {
+      x: cos * item.x - sin * item.y,
+      y: sin * item.x + cos * item.y,
+    };
+  }
+}
+
+export interface LineLike {
+  a: PointLike;
+  b: PointLike;
+}
+
+export class Line {
+  a: PointLike;
+  b: PointLike;
+  constructor(...params: [x0: number, y0: number, x1: number, y1: number] | [a: PointLike, b: PointLike] | [object: LineLike]) {
+    if (params.length === 4 && params.every((param) => typeof param === 'number')) {
+      this.a = {
+        x: params[0],
+        y: params[1],
+      };
+      this.b = {
+        x: params[2],
+        y: params[3],
+      };
+    } else if (params.length === 2 && params.every((param) => PointOrVector.is(param))) {
+      this.a = {
+        x: params[0].x,
+        y: params[0].y,
+      };
+      this.b = {
+        x: params[1].x,
+        y: params[1].y,
+      };
+    } else if (params.length === 1 && Line.is(params[0])) {
+      this.a = {
+        x: params[0].a.x,
+        y: params[0].a.y,
+      };
+      this.b = {
+        x: params[0].b.x,
+        y: params[0].b.y,
+      };
+    } else throw new Error('invalid constructor params');
+  }
+
+  intersection(other: LineLike, infinite?: boolean): PointLike | undefined {
+    return Line.intersection(this, other, infinite);
+  }
+
+  static intersection(item: LineLike, other: LineLike, infinite?: boolean): PointLike | undefined {
+    const denominator = (item.a.x - item.b.x) * (other.a.y - other.b.y) - (item.a.y - item.b.y) * (other.a.x - other.b.x);
+    if (denominator === 0) return;
+    const lengthA = ((item.a.x - other.a.x) * (other.a.y - other.b.y) - (item.a.y - other.a.y) * (other.a.x - other.b.x)) / denominator;
+    const lengthB = ((item.a.x - other.a.x) * (item.a.y - item.b.y) - (item.a.y - other.a.y) * (item.a.x - item.b.x)) / denominator;
+    return infinite || (lengthA >= 0 && lengthA <= 1 && lengthB >= 0 && lengthB <= 1)
+      ? { x: item.a.x + lengthA * (item.b.x - item.a.x), y: item.a.y + lengthA * (item.b.y - item.a.y) }
+      : undefined;
+  }
+
+  static is(item: unknown): item is PointLike {
+    return typeof item === 'object' && item !== null && 'a' in item && 'b' in item && PointOrVector.is(item.a) && PointOrVector.is(item.b);
   }
 }
